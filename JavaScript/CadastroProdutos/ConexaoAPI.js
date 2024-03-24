c30da561af56c6d302da4f2c07a4984cebcd5cc1
Lucas Gouveia <lucaspereiraa81@gmail.com>
2024-03-24 03:40:32 -0300

    Adequação para envio de imagens para a API

--- JavaScript/CadastroProdutos/ConexaoAPI.js
+++ JavaScript/CadastroProdutos/ConexaoAPI.js
@@ -1,34 +1,39 @@
 const formulario = document.querySelector("form");
 const Inome = document.querySelector(".nome");
 const Icategoria = document.querySelector(".categoria");
-const Ipreco = document.querySelector(".preco");
+// const Ipreco = document.querySelector(".preco");
+const Iimagem = document.querySelector("#picture__input"); // Novo
 
 function cadastrar() {
-    fetch("http://localhost:8080/api/cadastrarProd",
-    {
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
+    const formData = new FormData();
+
+    formData.append('nome', Inome.value);
+    formData.append('categoria', Icategoria.value);
+    // formData.append('preco', Ipreco.value);
+    formData.append('imagem', Iimagem.files[0]);
+
+    fetch("http://localhost:8080/api/cadastrarProd", {
         method: "POST",
-        body: JSON.stringify({
-            nome: Inome.value,
-            categoria: Icategoria.value,
-            preco: Ipreco.value
-        })
+        body: formData
     })
-    .then(function (res) { console.log(res) })
-    .catch(function (res) { console.log(res) })
+    .then(function (res) { 
+        console.log(res); 
+    })
+    .catch(function (res) { 
+        console.log(res); 
+    });
 }
+
 
 function limpar() {
     Inome.value = "";
     Icategoria.value = "";
-    Ipreco.value = "";
+    // Ipreco.value = "";
+    Iimagem.value = ""; // Limpe o campo de seleção de arquivo
 }
 
 formulario.addEventListener("submit", function (event){
     event.preventDefault();
     cadastrar();
     limpar();
-})
+});
