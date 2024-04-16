0197fa99b4adcaaeaf9af835b1b1f3992938716b
GabrielLomba12 <lombagabriel324@gmail.com>
2024-04-15 21:22:06 -0300

    fix validação tela login

--- JavaScript/Login/ConexaoAPI.js
+++ JavaScript/Login/ConexaoAPI.js
@@ -1,46 +1,40 @@
-const formulario = document.querySelector('.card-login').value
-const email = document.querySelector('.email').value
-const senha = document.querySelector('senha').value
+const formulario = document.querySelector("form")
+const email = document.querySelector(".email")
+const senha = document.querySelector(".senha")
 
-document.querySelector(".card-login").addEventListener("submit", function (event) {
-    event.preventDefault();
+// document.querySelector(".btn-login").addEventListener("submit", function (event) {
+//     event.preventDefault();
 
-    // const form = event.target;
-    // if (form.checkValidity()) {
-    //     const usuario = {
-    //         nome: form.querySelector("#nomeCompleto").value,
-    //         dtNascimento: form.querySelector("#dtNascimento").value,
-    //         cpf: form.querySelector("#cpf").value,
-    //         telefone: form.querySelector("#telefone").value,
-    //         email: form.querySelector("#email").value,
-    //         cep: form.querySelector("#cep").value,
-    //         logradouro: form.querySelector("#logradouro").value,
-    //         bairro: form.querySelector("#bairro").value,
-    //         numero: form.querySelector("#numero").value,
-    //         cidade: form.querySelector("#cidade").value,
-    //         uf: form.querySelector("#uf").value
-    //     };
+//     consultar(); // Recebe os dados do backend
+//     limparCampos(); // Limpa os campos do formulário após o envio bem-sucedido
+    
+// });
 
-        consultar(); // Recebe os dados do backend
-        limparCampos(); // Limpa os campos do formulário após o envio bem-sucedido
-    // }
-});
+
 
 function consultar() { 
 
+    const login = {
+        "email" : email.value,
+        "senha" : senha.value
+    };
+
+    console.log(login)
     fetch('http://localhost:8080/api/login', {
-        method: 'GET',
+        method: 'POST',
         headers: {
             'Content-Type': 'application/json'
-        }
+        },
+        body: JSON.stringify(login)
     })
         .then(response => {
             if (response.status === 200) {
                 // Login realizado com sucesso
-                setTimeout(() => {
-                    esconderLoading();
-                    document.querySelector(".").style.display = "flex";
-                }, 3000);
+                // setTimeout(() => {
+                //     esconderLoading();
+                     console.log('ok')
+                     document.querySelector(".card").style.display = "flex";
+                // }, 3000);
             } else if (response.status === 403) {
                 // CPF já cadastrado
                 alert("Usuário/senha inválido!");
@@ -50,12 +44,18 @@ function consultar() {
         })
         .catch(error => {
             // Exibe mensagem de erro em caso de falha na requisição
-            console.error('Erro ao acessar usuário:', error);
+            //console.error('Erro ao acessar usuário:', error);
             alert("Erro ao acessar usuário. Por favor, tente novamente.");
             esconderLoading(); // Esconde o modal de loading
             document.querySelector(".main").classList.remove('blur'); // Remove o efeito de blur
         });
 }
+
+formulario.addEventListener("submit", function (event){
+    event.preventDefault();
+
+    consultar();
+})
 
 function limparCampos() {
     document.querySelector(".card-login").reset();
