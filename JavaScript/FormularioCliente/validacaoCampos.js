424ef865dac8d4ab4df5dce4c9cbab6882fe761d
jcavalcantee <jcavalcantes@gmail.com>
2024-04-17 20:22:34 -0300

    continuacao da validacao

--- JavaScript/FormularioCliente/validacaoCampos.js
+++ JavaScript/FormularioCliente/validacaoCampos.js
@@ -35,14 +35,21 @@ document.getElementById("telefone").addEventListener("input", function () {
     this.value = formattedPhone;
 });
 
-function validarAno() {
+document.getElementById("dtNascimento").addEventListener("change", function () {
+    validarData();
+});
+
+function validarData() {
     var inputDate = document.getElementById("dtNascimento").value;
     var selectedDate = new Date(inputDate);
     var selectedYear = selectedDate.getFullYear();
 
     if (selectedYear < 1900 || selectedYear > 2024) {
-        alert("Por favor, selecione um ano entre 1900 e 2024.");
-        document.getElementById("dtNascimento").value = "";
+        document.getElementById("dtNascimento").classList.add("is-invalid");
+        document.querySelector("#dtNascimento + .invalid-feedback").textContent = "Informe uma data de nascimento entre 1900 e 2024.";
+    } else {
+        document.getElementById("dtNascimento").classList.remove("is-invalid");
+        document.querySelector("#dtNascimento + .invalid-feedback").textContent = "";
     }
 }
 
@@ -66,27 +73,27 @@ document.getElementById("cep").addEventListener("input", function () {
     }
 });
 
-document.getElementById("senha").addEventListener("blur", function () {
-    var senha = this.value;
-
-    if (senha.length < 5) {
-        this.classList.add("is-invalid");
-        this.nextElementSibling.textContent = "A senha deve conter no mínimo 5 caracteres.";
-    } else {
-        this.classList.remove("is-invalid");
-        this.nextElementSibling.textContent = "";
-    }
-});
-
-document.getElementById("confirmSenha").addEventListener("blur", function () {
+function validarSenha() {
     var senha = document.getElementById("senha").value;
-    var confirmSenha = this.value;
+    var confirmSenha = document.getElementById("confirmSenha").value;
 
-    if (senha !== confirmSenha) {
-        this.classList.add("is-invalid");
-        this.nextElementSibling.textContent = "As senhas não correspondem.";
+    if (senha.length < 5 || confirmSenha.length < 5) {
+        document.getElementById("senha").classList.add("is-invalid");
+        document.getElementById("confirmSenha").classList.add("is-invalid");
+        document.querySelector("#confirmSenha + .invalid-feedback").textContent = "A senha deve ter no mínimo 5 caracteres.";
+        return false;
+    } else if (senha !== confirmSenha) {
+        document.getElementById("senha").classList.add("is-invalid");
+        document.getElementById("confirmSenha").classList.add("is-invalid");
+        document.querySelector("#confirmSenha + .invalid-feedback").textContent = "As senhas devem ser iguais.";
+        return false;
     } else {
-        this.classList.remove("is-invalid");
-        this.nextElementSibling.textContent = "";
+        document.getElementById("senha").classList.remove("is-invalid");
+        document.getElementById("confirmSenha").classList.remove("is-invalid");
+        document.querySelector("#confirmSenha + .invalid-feedback").textContent = "";
     }
-});
+}
+
+document.getElementById("senha").addEventListener("input", validarSenha);
+
+document.getElementById("confirmSenha").addEventListener("input", validarSenha)
