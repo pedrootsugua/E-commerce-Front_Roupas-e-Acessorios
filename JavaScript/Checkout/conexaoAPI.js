05434b8644f86db0af96f06c5f9dd549b7632132
jcavalcantee <jcavalcantes@gmail.com>
2024-05-10 23:14:40 -0300

    realizando o cadastro de novos enderecos do usuario no checkout do pedido

--- JavaScript/Checkout/conexaoAPI.js
+++ JavaScript/Checkout/conexaoAPI.js
@@ -1,13 +1,26 @@
 usuarioAutenticado();
 
-document.addEventListener("DOMContentLoaded", function () {
-    const checkbox = document.getElementById("check");
+var params = new URLSearchParams(window.location.search);
+var mensagem = params.get('mensagem');
 
-    checkbox.addEventListener("change", function () {
-        if (this.checked) {
+const form = document.querySelector('.formulario')
+
+document.addEventListener("DOMContentLoaded", function () {
+    const checkboxEnderecoSalvo = document.getElementById("check");
+    const checkboxNovoEndereco = document.getElementById("check1");
+
+    checkboxEnderecoSalvo.addEventListener("change", function () {
+        if (checkboxEnderecoSalvo.checked) {
             consultarEnderecoUsuario(id);
-        } else
+        } else {
             limparCampos();
+        }
+    });
+
+    checkboxNovoEndereco.addEventListener("change", function () {
+        if (checkboxNovoEndereco.checked) {
+            cadastrarNovoEndereco();
+        }
     });
 });
 
@@ -22,12 +35,40 @@ function consultarEnderecoUsuario(id) {
             return response.json();
         })
         .then(data => {
-            // console.log(data);
             // Pega os dados retornados da API e concatena para exibir no campo.
             document.getElementById("enderecoSalvo")
                 .value = data.logradouro + ', ' + data.numero + ' - ' +
                 data.bairro + ' (' + data.cidade + ')';
 
+        })
+        .catch(error => {
+            console.log("Erro: " + error);
+        })
+}
+
+function cadastrarNovoEndereco() {
+
+    const novoEndereco = {
+        cep: form.querySelector("#cep").value,
+        logradouro: form.querySelector("#logradouro").value,
+        bairro: form.querySelector("#bairro").value,
+        numero: form.querySelector("#numero").value,
+        cidade: form.querySelector("#cidade").value,
+        uf: form.querySelector("#uf").value,
+        idUsuario: mensagem
+    };
+    fetch(`http://localhost:8080/api/enderecos/novo`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(novoEndereco)
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Erro ao acessar a API: " + response.statusText);
+            }
+            return response.json();
         })
         .catch(error => {
             console.log("Erro: " + error);
@@ -50,13 +91,39 @@ function usuarioAutenticado() {
             const autenticado = data.autenticado;
             id = data.credencialModel.idUsuario;
             if (autenticado === true) {
-                document.getElementById("login_user").innerHTML = "Olá, usuário.";
-                // consultarEnderecoUsuario(id);
+                document.getElementById("login_user").innerHTML = "Olá";
+                buscarUsuario(id);
             }
-            // e assim por diante, dependendo dos campos do objeto retornado
         })
         .catch(error => {
             console.error('Erro:', error);
+            alert("Erro ao acessar usuário. Por favor, tente novamente.");
+        });
+}
+
+function buscarUsuario(id) {
+    console.log(id)
+    fetch('http://localhost:8080/api/usuarios/' + id, {
+        method: 'GET',
+    })
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error('Erro ao fazer login');
+            }
+        })
+        .then(data => {
+            console.log(data);
+            nome = data.nome;
+            // Divida a string em um array de palavras usando o espaço como delimitador
+            let palavras = nome.split(" ");
+            // Acesse a primeira palavra, que está no índice 0 do array
+            let primeiroNome = palavras[0];
+            document.getElementById("login_user").innerHTML = "Olá, " + primeiroNome;
+        })
+        .catch(error => {
+            console.error('Erro ao fazer login:', error);
             alert("Erro ao acessar usuário. Por favor, tente novamente.");
         });
 }
--- JavaScript/Checkout/consultaAPIexterna.js
+++ JavaScript/Checkout/consultaAPIexterna.js
@@ -2,6 +2,7 @@ function limpa_formulário_cep() {
     document.getElementById("logradouro").value = ("");
     document.getElementById("bairro").value = ("");
     document.getElementById("cidade").value = ("");
+    document.getElementById("uf").value = ("");
 }
 
 function meu_callback(conteudo) {
@@ -9,6 +10,7 @@ function meu_callback(conteudo) {
         document.getElementById("logradouro").value = (conteudo.logradouro);
         document.getElementById("bairro").value = (conteudo.bairro);
         document.getElementById("cidade").value = (conteudo.localidade);
+        document.getElementById("uf").value = (conteudo.uf);
     }
     else {
         limpa_formulário_cep();
@@ -30,6 +32,7 @@ function pesquisacep(cepInput) {
             document.getElementById("logradouro").value = "...";
             document.getElementById("bairro").value = "...";
             document.getElementById("cidade").value = "...";
+            document.getElementById("uf").value = "..."
 
             var script = document.createElement('script');
 
