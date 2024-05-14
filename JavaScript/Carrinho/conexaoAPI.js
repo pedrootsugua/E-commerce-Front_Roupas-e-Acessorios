11a4ab7a46019ebd6aada7be02b838db07c1eeb5
jcavalcantee <jcavalcantes@gmail.com>
2024-05-14 18:14:39 -0300

    alteracoes no JS

--- JavaScript/Carrinho/conexaoAPI.js
+++ JavaScript/Carrinho/conexaoAPI.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Recuperar a string da URL
     var params = new URLSearchParams(window.location.search);
-    var mensagem = params.get('mensagem');
+    var userId = params.get('userId');
     document.querySelector('.botao-cont').addEventListener('click', function (event) {
         event.preventDefault(); // Evita o comportamento padrão do formulário
         // Enviar a string para a outra tela como parâmetro na URL
-        window.location.href = 'TelaCheckout.html?mensagem=' + mensagem;
+        window.location.href = 'TelaCheckout.html?userId=' + userId;
     });
 });
