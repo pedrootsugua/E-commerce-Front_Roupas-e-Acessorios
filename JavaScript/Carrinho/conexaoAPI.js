9fa08b27ebf1fbd4ce92c08ed244c6082e12cb26
pedrootsugua <pedro.zanele@gmail.com>
2024-05-16 00:03:32 -0300

    conectando front do carrinho com a API

--- JavaScript/Carrinho/conexaoAPI.js
+++ JavaScript/Carrinho/conexaoAPI.js
@@ -8,3 +8,46 @@ document.addEventListener('DOMContentLoaded', function () {
         window.location.href = 'TelaCheckout.html?userId=' + userId;
     });
 });
+
+document.querySelector(".carrinho").addEventListener("click", function (event) {
+    event.preventDefault();
+    acessarCarrinhoProduto(id);
+    console.log(id);
+    window.location.href = "TelaCarrinho.html";
+});
+
+function acessarCarrinhoProduto (id) {
+    fetch(`http://localhost:8080/api/carrinho?id=${id}`, {
+        method: 'GET'
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Erro ao acessar a API: " + response.statusText);
+            }
+            return response.json();
+        })
+        .then(data => {
+            let carrinhoVazio = document.getElementById("empty-cart");
+            let carrinhoComProduto = document.getElementById("product-cart");
+            let listaProdutos = data.quantidadeItens;
+            if(listaProdutos != 0) {
+                carrinhoComProduto.style.display = "flex";
+            } else {
+                carrinhoVazio.style.display = "flex";
+            }
+            // const selectEndereco = document.getElementById("endereco");
+            // selectEndereco.innerHTML = "";
+
+            // data.forEach(endereco => {
+            //     const option = document.createElement("option");
+            //     option.value = JSON.stringify(endereco); // Armazena o endereço completo como valor da opção
+            //     option.text = `${endereco.logradouro}, ${endereco.numero} - ${endereco.bairro}, ${endereco.cidade}, ${endereco.uf}`;
+            //     selectEndereco.appendChild(option);
+            // });
+
+            // exibirEnderecoSelecionado();
+        })
+        .catch(error => {
+            console.log("Erro: " + error);
+        })
+}
