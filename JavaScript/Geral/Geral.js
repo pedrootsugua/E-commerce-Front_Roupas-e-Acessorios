72e0f62de8da81f822b3806cc8331aa88a841b04
GabrielLomba12 <lombagabriel324@gmail.com>
2024-06-05 23:58:02 -0300

    estilização tela inicial e direcionamento de telas corrigido

--- JavaScript/Geral/Geral.js
+++ JavaScript/Geral/Geral.js
@@ -46,6 +46,12 @@ document.addEventListener('DOMContentLoaded', function () {
         console.log(4)
         deslogar();
     });
+    document.getElementById('confira').addEventListener('click', function (event) {
+        event.preventDefault(); // Evita o comportamento padrão do formulário
+        // Enviar a string para a outra tela como parâmetro na URL
+        // var mensagem = id;
+        window.location.href = 'DetalheProduto.html?produtoId=20';
+    });
 
 });
 
